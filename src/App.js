66c7b904f41d8e772e8a166c171c43ae4cf54b37
unknown <novita9611@gmail.com>
2022-03-12 13:58:31 +0800

    Add Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 // CSS Settings
 import { Header, AppNameComp, SearchComp, SearchInput } from './components/headerComponents';
+import Footer from './components/Footer';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -146,6 +147,7 @@ function App() {
             <SeeCompleteLabel>See Complete Recipe</SeeCompleteLabel>
         </RecipeContainer>
       </RecipeList>
+      <Footer />
     </Container>
   );
 }
